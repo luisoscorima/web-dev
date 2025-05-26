d0f8aa3716f6ee9248551d40b504b4e6ef1f362f
LuPalacios-14 <1601142@utp.edu.pe>
2025-05-26 04:30:16 -0500

    changes

---
+++ src/main/java/pe/edu/utp/isi/dwi/proyectodwi/model/Cliente.java
@@ -0,0 +1,68 @@
+
+package pe.edu.utp.isi.dwi.proyectodwi.model;
+
+public class Cliente {
+    private int idCliente;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String tipoCliente;
+    private String estado;
+    private int idEmpresa;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(int idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+}
