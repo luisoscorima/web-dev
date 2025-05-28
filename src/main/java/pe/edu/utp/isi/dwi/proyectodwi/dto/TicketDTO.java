8a2ea1d0ee88df81450563fa4573615cbe744dc8
LUIS GUSTAVO OSCORIMA PALOMINO <100874269+luisoscorima@users.noreply.github.com>
2025-05-28 17:46:06 -0500

    Update TicketDTO.java

--- src/main/java/pe/edu/utp/isi/dwi/proyectodwi/dto/TicketDTO.java
+++ src/main/java/pe/edu/utp/isi/dwi/proyectodwi/dto/TicketDTO.java
@@ -1,38 +1,28 @@
 package pe.edu.utp.isi.dwi.proyectodwi.dto;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public class TicketDTO {
-    private int idSolicitud;
+    private int id_solicitud;
     private String asunto;
-    private String estado;
     private String prioridad;
-    private LocalDateTime fechaCreacion;
-    private boolean esCoordinador;
+    private Date fechaCreacion;
+    private String estado;
 
-    // getters y setters
+    // Getters y setters
+    public int getId() { return id_solicitud; }
+    public void setId(int id) { this.id_solicitud = id; }
 
-    public void setAsunto(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public String getAsunto() { return asunto; }
+    public void setAsunto(String asunto) { this.asunto = asunto; }
 
-    public void setEstado(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public String getPrioridad() { return prioridad; }
+    public void setPrioridad(String prioridad) { this.prioridad = prioridad; }
 
-    public void setIdSolicitud(int aInt) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public Date getFechaCreacion() { return fechaCreacion; }
+    public void setFechaCreacion(Date fechaCreacion) { this.fechaCreacion = fechaCreacion; }
 
-    public void setPrioridad(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public void setFechaCreacion(LocalDateTime toLocalDateTime) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public void setEsCoordinador(boolean aBoolean) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
 }
